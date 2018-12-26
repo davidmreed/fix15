d3eb25fbe524ec4ed5fb8694d516d9eb4af4cfe5
David Reed <david@ktema.org>
2018-12-26 14:52:06 -0500

    Add GitHub URL

--- setup.py
+++ setup.py
@@ -6,10 +6,22 @@ setup(
     description='Convert 15-character Salesforce Ids to 18-character Ids in CSV files.',
     author='David Reed',
     author_email='david@ktema.org',
+    url='https://github.com/davidmreed/fix15',
     license='MIT License',
     packages=['fix15'],
     test_suite='nose.collector',
     tests_require=['nose'],
+    keywords=['Salesforce'],
+    platforms='Any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities'
+    ],
     entry_points={
         'console_scripts': [
             'fix15 = fix15.__main__:main'
